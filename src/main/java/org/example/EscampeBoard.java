664c6c257b1bfeafad1826225bc301d567cba26a
Maxime Fromont <maxime.fromont0@gmail.com>
2024-05-21 14:42:04 +0200

    Added board initialization and print

--- src/main/java/org/example/EscampeBoard.java
+++ src/main/java/org/example/EscampeBoard.java
@@ -7,10 +7,51 @@ public class EscampeBoard implements Partie1 {
 
     //CONSTRUCTOR
     public EscampeBoard() {
-        this.board = new Case[BOARD_SIZE][BOARD_SIZE];
+        board = new Case[BOARD_SIZE][BOARD_SIZE];
 
-        //Initialise cases (and their values)
-        
+        //Initializing the board with straight values from the subject
+        //1st line
+        board[0][0] = new Case(1);
+        board[0][1] = new Case(2);
+        board[0][2] = new Case(2);
+        board[0][3] = new Case(3);
+        board[0][4] = new Case(1);
+        board[0][5] = new Case(2);
+        //2nd line
+        board[1][0] = new Case(3);
+        board[1][1] = new Case(1);
+        board[1][2] = new Case(3);
+        board[1][3] = new Case(1);
+        board[1][4] = new Case(3);
+        board[1][5] = new Case(2);
+        //3rd line
+        board[2][0] = new Case(2);
+        board[2][1] = new Case(3);
+        board[2][2] = new Case(1);
+        board[2][3] = new Case(2);
+        board[2][4] = new Case(1);
+        board[2][5] = new Case(3);
+        //4th line
+        board[3][0] = new Case(2);
+        board[3][1] = new Case(1);
+        board[3][2] = new Case(3);
+        board[3][3] = new Case(2);
+        board[3][4] = new Case(3);
+        board[3][5] = new Case(1);
+        //5th line
+        board[4][0] = new Case(1);
+        board[4][1] = new Case(3);
+        board[4][2] = new Case(1);
+        board[4][3] = new Case(3);
+        board[4][4] = new Case(1);
+        board[4][5] = new Case(2);
+        //6th line
+        board[5][0] = new Case(3);
+        board[5][1] = new Case(2);
+        board[5][2] = new Case(2);
+        board[5][3] = new Case(1);
+        board[5][4] = new Case(3);
+        board[5][5] = new Case(2);
     }
 
     @Override
@@ -41,6 +82,15 @@ public class EscampeBoard implements Partie1 {
     @Override
     public boolean gameOver() {
         return false;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(board[i][j].getValue() + " ");
+            }
+            System.out.println();
+        }
     }
 
     //PRIVATE CONSTANTS
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        EscampeBoard board = new EscampeBoard();
+        EscampeBoard escampeBoard = new EscampeBoard();
+        escampeBoard.printBoard();
     }
 }
