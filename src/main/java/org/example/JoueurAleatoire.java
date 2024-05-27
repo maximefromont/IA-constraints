4647a76d54895480a3e99d8d3adfb9feba0a5e97
Maxime <maxime.fromont0@gmail.com>
2024-05-27 12:30:13 +0200

    Finished joueur

--- src/main/java/org/example/JoueurAleatoire.java
+++ src/main/java/org/example/JoueurAleatoire.java
@@ -1,14 +1,35 @@
 package org.example;
 
+import org.example.enums.TEAM_COLOR;
+import org.example.move.Move;
+import org.example.move.RegularMove;
+
 public class JoueurAleatoire implements IJoueur {
+
+    //PRIVATE ATTRIBUTES
+    private TEAM_COLOR playerColor;
+    private EscampeBoard board; //This is a reference to the board that is instancied in the main
+
+
+    //CONSTRUCTOR
+    public JoueurAleatoire(EscampeBoard board) {
+        this.board = board; //The board is instancied in the constructor, however it's important that the referee call the initJoueur method before actually starting to play
+    }
+
     @Override
     public void initJoueur(int mycolour) {
-        
+        this.playerColor = TEAM_COLOR.getTeamColorFromInt(mycolour);
     }
 
     @Override
     public int getNumJoueur() {
-        return 0;
+        if (playerColor == TEAM_COLOR.WHITE_TEAM) {
+            return TEAM_COLOR.WHITE_TEAM_INT;
+        } else if (playerColor == TEAM_COLOR.BLACK_TEAM){
+            return TEAM_COLOR.BLACK_TEAM_INT;
+        }
+
+        return 0; //By default 0 = error
     }
 
     @Override
@@ -18,12 +39,18 @@ public class JoueurAleatoire implements IJoueur {
 
     @Override
     public void declareLeVainqueur(int colour) {
-
+        TEAM_COLOR winningCollor = TEAM_COLOR.getTeamColorFromInt(colour);
+        if (winningCollor == playerColor) {
+            System.out.println("What a beautiful day, I have zero problems in life \uD83E\uDD13 !");
+        } else {
+            System.out.println("What a terrible day, I have only problems in life \uD83D\uDE3E !");
+        }
     }
 
     @Override
     public void mouvementEnnemi(String coup) {
-
+        TEAM_COLOR ennemiColor = TEAM_COLOR.getOppositeTeamColor(playerColor);
+        board.play(coup, TEAM_COLOR.getTeamColorStringFromTeamColor(ennemiColor));
     }
 
     @Override
--- src/main/java/org/example/enums/TEAM_COLOR.java
+++ src/main/java/org/example/enums/TEAM_COLOR.java
@@ -1,23 +1,52 @@
 package org.example.enums;
 
-import org.example.Printinator;
-
 public enum TEAM_COLOR {
     BLACK_TEAM,
     WHITE_TEAM;
 
 
     public static TEAM_COLOR getTeamColorFromString(String teamColor) {
-        if (teamColor.equals(WHITE_TEAM_LOWERCASE)) {
+        if (teamColor.equals(WHITE_TEAM_STRING)) {
             return WHITE_TEAM;
-        } else if (teamColor.equals(BLACK_TEAM_LOWERCASE)) {
+        } else if (teamColor.equals(BLACK_TEAM_STRING)) {
             return BLACK_TEAM;
         }
 
-        return null;
+        return null; //By default null = error
+    }
+
+    public static TEAM_COLOR getTeamColorFromInt(int teamColor) {
+        if (teamColor == WHITE_TEAM_INT) {
+            return WHITE_TEAM;
+        } else if (teamColor == BLACK_TEAM_INT) {
+            return BLACK_TEAM;
+        }
+
+        return null; //By default null = error
+    }
+
+    public static TEAM_COLOR getOppositeTeamColor(TEAM_COLOR teamColor) {
+        if (teamColor == WHITE_TEAM) {
+            return BLACK_TEAM;
+        } else {
+            return WHITE_TEAM;
+        }
+    }
+
+    public static String getTeamColorStringFromTeamColor(TEAM_COLOR teamColor) {
+        if (teamColor == WHITE_TEAM) {
+            return WHITE_TEAM_STRING;
+        } else if (teamColor == BLACK_TEAM) {
+            return BLACK_TEAM_STRING;
+        }
+
+        return null; //By default null = error
     }
 
     //PUBLIC CONSTANTS
-    public static final String WHITE_TEAM_LOWERCASE = "blanc";
-    public static final String BLACK_TEAM_LOWERCASE = "noir";
+    public static final String WHITE_TEAM_STRING = "blanc";
+    public static final String BLACK_TEAM_STRING = "noir";
+
+    public static final int WHITE_TEAM_INT = -1;
+    public static final int BLACK_TEAM_INT = 1;
 }
