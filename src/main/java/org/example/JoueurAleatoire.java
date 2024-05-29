7202ab9871992417cf1198c22319eb3fd1bfbbf8
MarreTeint <martinzs1@yahoo.fr>
2024-05-29 17:05:05 +0200

    [Add] Side selection on JoueurAleatoire

--- src/main/java/org/example/JoueurAleatoire.java
+++ src/main/java/org/example/JoueurAleatoire.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.enums.TEAM_COLOR;
 import org.example.move.Move;
+import org.example.move.PositionMove;
 import org.example.move.RegularMove;
 
 import java.util.ArrayList;
@@ -13,6 +14,35 @@ public class JoueurAleatoire implements IJoueur {
     private TEAM_COLOR playerColor;
     private EscampeBoard board; //This is a reference to the board that is instancied in the main
 
+    private ArrayList<String> upperStart = new ArrayList<>() {{
+        add("A1");
+        add("B1");
+        add("C1");
+        add("D1");
+        add("E1");
+        add("F1");
+        add("A2");
+        add("B2");
+        add("C2");
+        add("D2");
+        add("E2");
+        add("F2");
+    }};
+
+    private ArrayList<String> lowerStart = new ArrayList<>() {{
+        add("A5");
+        add("B5");
+        add("C5");
+        add("D5");
+        add("E5");
+        add("F5");
+        add("A6");
+        add("B6");
+        add("C6");
+        add("D6");
+        add("E6");
+        add("F6");
+    }};
 
     //CONSTRUCTOR
     public JoueurAleatoire() {
@@ -39,47 +69,29 @@ public class JoueurAleatoire implements IJoueur {
 
     @Override
     public String choixMouvement() {
-        System.out.println(board.playedCoups());
-        if(board.playedCoups() == 0){
-            ArrayList<String> availablesCases = new ArrayList<>();
-            String move = "";
-            availablesCases.add("A1");
-            availablesCases.add("B1");
-            availablesCases.add("C1");
-            availablesCases.add("D1");
-            availablesCases.add("E1");
-            availablesCases.add("F1");
-            availablesCases.add("A2");
-            availablesCases.add("B2");
-            availablesCases.add("C2");
-            availablesCases.add("D2");
-            availablesCases.add("E2");
-            availablesCases.add("F2");
-            for (int i = 0; i<6 ; i++){
-                int place = (int) (Math.random() * availablesCases.size());
-                move = move + availablesCases.get(place);
-                availablesCases.remove(place);
-                if(i != 5){
-                    move = move + "/";
+        if(board.playedCoups()<=1){
+            boolean goLower;
+            if(playerColor == TEAM_COLOR.BLACK_TEAM){
+                goLower = Math.random() < 0.5;
+            }
+            else{
+                // check if all the cases in upperStart are empty
+                goLower = false;
+                for (String s : upperStart){
+                    Coordinate c = new Coordinate(s);
+                    if(!board.isFree(c)){
+                        System.out.println("upperStart is not free : "+ s);
+                        goLower = true;
+                        break;
+                    }
                 }
             }
-            board.play(move, TEAM_COLOR.getTeamColorStringFromTeamColor(playerColor));
-            return move;
-        } else if (board.playedCoups() == 1) {
-            ArrayList<String> availablesCases = new ArrayList<>();
+            ArrayList<String> availablesCases = upperStart;
+            if(goLower){
+                availablesCases = lowerStart;
+            }
             String move = "";
-            availablesCases.add("A5");
-            availablesCases.add("B5");
-            availablesCases.add("C5");
-            availablesCases.add("D5");
-            availablesCases.add("E5");
-            availablesCases.add("F5");
-            availablesCases.add("A6");
-            availablesCases.add("B6");
-            availablesCases.add("C6");
-            availablesCases.add("D6");
-            availablesCases.add("E6");
-            availablesCases.add("F6");
+
             for (int i = 0; i<6 ; i++){
                 int place = (int) (Math.random() * availablesCases.size());
                 move = move + availablesCases.get(place);
